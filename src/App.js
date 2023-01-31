2550607ea680d62186074ef66f1f9d6118b29f92
Tushar Kumar <tusharkumar@192.168.1.9>
2023-01-31 20:34:53 +0530

    [NEW] added the class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './modules/home/Home';
+import Services from './modules/Services/Services';
+
 
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Services/>
     </div>
   );
 }
---
+++ src/modules/Services/Services.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+class Services extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                This is the react app
+            </div>
+        )
+    }
+}
+
+export default Services;
